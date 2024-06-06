1c3eb6ac1e9ffe860d303d365ad3cbfab20f991d
Wilk <s197653@student.pg.edu.pl>
2024-06-06 18:33:14 +0200

    klonowanie repozytorium

---
+++ tp_projekt_4/out/build/x64-Debug/CMakeFiles/CMakeScratch/TryCompile-nklszh/src.cxx
@@ -0,0 +1,9 @@
+        #include <cstdlib>
+        #include <filesystem>
+
+        int main() {
+            auto cwd = std::filesystem::current_path();
+            printf("%s", cwd.c_str());
+            return EXIT_SUCCESS;
+        }
+    
