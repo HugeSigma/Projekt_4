51c9fcf8c405e7e22aa4c434909b63ffd6ac9993
Wilk <s197653@student.pg.edu.pl>
2024-06-08 23:17:19 +0200

    wykresy po naciśnięciu p; po narysowaniu wykresu można powrócić do zabawy dronem po wprowadeniu "enter" w konsoli

--- tp_projekt_4/out/build/x64-Debug/SDL/include/SDL_revision.h
+++ tp_projekt_4/out/build/x64-Debug/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/HugeSigma/Projekt_4@3f5fe91da5306c26247e44ecb4181b8ecb890f2b"
+#define SDL_REVISION "https://github.com/HugeSigma/Projekt_4@8fec40be5ba00ce435f63e9a5ac739ad97efa0d7"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
