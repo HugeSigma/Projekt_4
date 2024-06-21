84a030eaa6688712f571543172d2c8e4c5040070
HugeSigma <166825856+HugeSigma@users.noreply.github.com>
2024-06-21 17:18:31 +0200

    Delete tp_projekt_4/build directory
    usuniecie plikow z folderu build

--- tp_projekt_4/build/SDL/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/HugeSigma/Projekt_4@f48d9abeab1074e67c54b199b880e8e878dcfbfb"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
