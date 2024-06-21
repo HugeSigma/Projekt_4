2ffdcb064957cb9bbf68dd9672d5dfc24f6df925
HugeSigma <166825856+HugeSigma@users.noreply.github.com>
2024-06-21 17:20:16 +0200

    Delete tp_projekt_4/out/build/x64-Debug directory
    usuniecie plikow z folderu out/build

--- tp_projekt_4/out/build/x64-Debug/SDL/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/HugeSigma/Projekt_4@8fec40be5ba00ce435f63e9a5ac739ad97efa0d7"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
